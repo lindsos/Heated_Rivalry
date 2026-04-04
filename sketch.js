5125c49b19912a06c4fafdc964fbbba236d532d2
lindsos <lindseystone04@gmail.com>
2026-04-04 16:59:34 -0400

    detail fixing

--- sketch.js
+++ sketch.js
@@ -16,31 +16,31 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  link1 = createA("index1.html", "Episode 1- ROOKIES");
-  link2 = createA("index2.html", "Episode 2- OLYMPIANS");
-  link3 = createA("index3.html", "Episode 3- HUNTER");
-  link4 = createA("index4.html", "Episode 4- ROSE");
-  link5 = createA("index5.html", "Episode 5- ILL BELIEVE IN ANYTHING");
-  link6 = createA("index6.html", "Episode 6- THE COTTAGE");
+  link1 = createA("index1.html", "EPISODE 1- ROOKIES");
+  link2 = createA("index2.html", "EPISODE 2- OLYMPIANS");
+  link3 = createA("index3.html", "EPISODE 3- HUNTER");
+  link4 = createA("index4.html", "EPISODE 4- ROSE");
+  link5 = createA("index5.html", "EPISODE 5- ILL BELIEVE IN ANYTHING");
+  link6 = createA("index6.html", "EPISODE 6- THE COTTAGE");
 }
 
 function draw() {
   background("white");
 
   //title
-  textSize(75);
+  textSize(120);
   textAlign(CENTER);
   fill("black");
-  text("HEATED RIVALRY", 350, 85);
-  textFont("zumme-cut");
+  text("HEATED RIVALRY", 350, 105);
+  textFont("zuume-cut");
   textStyle(BOLD);
 
   //episode 1
-  link1.position(25, 275);
+  link1.position(25, 270);
   link1.style("font-size", "10px");
-  link1.style(textAlign(LEFT));
+  link1.style("text-align", "LEFT");
+  link1.style("text-style", "NORMAL");
   link1.style("color", "black");
-  link1.style("z-index", "5");
   link1.style("text-decoration", "none");
   link1.style("font-family", "zumme");
   noStroke();
@@ -49,11 +49,11 @@ function draw() {
   image(img1, 25, 130, 190, 135);
 
   //episode 2
-  link2.position(250, 275);
+  link2.position(250, 270);
   link2.style("font-size", "10px");
-  link2.style(textAlign(LEFT));
+  link2.style("text-align", "LEFT");
+  link2.style("text-style", "NORMAL");
   link2.style("color", "black");
-  link2.style("z-index", "5");
   link2.style("text-decoration", "none");
   link2.style("font-family", "zumme");
   noStroke();
@@ -62,11 +62,11 @@ function draw() {
   image(img2, 250, 140, 190, 125);
 
   //episode 3
-  link3.position(475, 275);
+  link3.position(475, 270);
   link3.style("font-size", "10px");
-  link3.style(textAlign(LEFT));
+  link3.style("text-align", "LEFT");
+  link3.style("text-style", "NORMAL");
   link3.style("color", "black");
-  link3.style("z-index", "5");
   link3.style("text-decoration", "none");
   link3.style("font-family", "zumme");
   noStroke();
@@ -75,11 +75,11 @@ function draw() {
   image(img3, 475, 125, 190, 140);
 
   //episode 4
-  link4.position(25, 465);
+  link4.position(25, 460);
   link4.style("font-size", "10px");
-  link4.style(textAlign(LEFT));
+  link4.style("text-align", "LEFT");
+  link4.style("text-style", "NORMAL");
   link4.style("color", "black");
-  link4.style("z-index", "5");
   link4.style("text-decoration", "none");
   link4.style("font-family", "zumme");
   noStroke();
@@ -88,11 +88,11 @@ function draw() {
   image(img4, 25, 315, 190, 140);
 
   //episode 5
-  link5.position(250, 465);
+  link5.position(250, 460);
   link5.style("font-size", "10px");
-  link5.style(textAlign(LEFT));
+  link5.style("text-align", "LEFT");
+  link5.style("text-style", "NORMAL");
   link5.style("color", "black");
-  link5.style("z-index", "5");
   link5.style("text-decoration", "none");
   link5.style("font-family", "zumme");
   noStroke();
@@ -101,11 +101,11 @@ function draw() {
   image(img5, 250, 315, 190, 140);
 
   //episode 6
-  link6.position(475, 465);
+  link6.position(475, 460);
   link6.style("font-size", "10px");
-  link6.style(textAlign(LEFT));
+  link6.style("text-align", "LEFT");
+  link6.style("text-style", "NORMAL");
   link6.style("color", "black");
-  link6.style("z-index", "5");
   link6.style("text-decoration", "none");
   link6.style("font-family", "zumme");
   noStroke();
--- sketch2.js
+++ sketch2.js
@@ -2,27 +2,26 @@ let img16;
 let img17;
 let img18;
 let img19;
-var backgrounds = ["background1", "background2"];
-var currentImage;
+var backgrounds = [];
+let index = 0;
 
 function preload() {
-  img16 = loadImage("gif_4.png");
-  img17 = loadImage("gif_5.png");
-  img18 = loadImage("gif_6.png");
-  img19 = loadImage("tv_remote.png");
+  img16 = loadImage("assets/episode_2/gif_4.png");
+  img17 = loadImage("assets/episode_2/gif_5.png");
+  img18 = loadImage("assets/episode_2/gif_6.png");
+  img19 = loadImage("assets/tv_remote.png");
+  backgrounds[0] = loadImage("assets/episode_2/episode_2_background.png");
+  backgrounds[1] = loadImage("assets/episode_2/episode_2_background_2.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   link = createA("index.html", "Return to home");
-  background1 = loadImage("episode_2_background.png");
-  background2 = loadImage("episode_2_background_2.png");
-  currentImage = "background1";
 }
 
 function draw() {
   background("#111C45");
-  image(currentImage, 0, 75, 700, 425);
+  image(backgrounds[index], 0, 75, 700, 425);
   textSize(12);
   textAlign(CENTER);
   fill("white");
@@ -39,27 +38,21 @@ function draw() {
   image(img16, 505, 75, 110, 60);
   image(img17, 75, 75, 95, 70);
   image(img18, 315, 400, 115, 70);
-  link.position(40, 55); 
-  link.style('font-size', '10px');
-  link.style('color', 'white');
-  link.style('z-index', '5'); 
-  link.style('text-decoration', 'none'); 
-  link.style('font-family', 'helvetica-neue-lt-pro');
-  image(img19, 25, 10, 35, 35);
+  image(img19, 30, 10, 35, 35);
+  link.position(10, 45);
+  link.style("font-size", "10px");
+  link.style("text-align", "CENTER");
+  link.style("text-style", "NORMAL");
+  link.style("color", "white");
+  link.style("text-decoration", "none");
+  link.style("font-family", "helvetica-neue-lt-pro");
 }
 
 function mousePressed() {
-  if (backgrounds == 0) {
-    currentImage = "background1";
-    console.log("background1");
-  } else if ("background2" == 1) {
-    currentImage = "background2";
-    console.log("background2");
-  } else {
-    currentImage = "background1";
-    backgrounds = -1;
+  index = index + 1;
+  if (index == backgrounds.length) {
+    index = 0;
   }
-  backgrounds = backgrounds + 1;
 }
 
 function windowResized() {
