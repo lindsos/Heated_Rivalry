6da736d33705eddaa991cb458c32cd590bb45592
lindsos <lindseystone04@gmail.com>
2026-04-06 21:14:31 -0400

    resize episode 2

--- sketch2.js
+++ sketch2.js
@@ -15,32 +15,32 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1600, 900);
   link = createA("index.html", "Return to home");
 }
 
 function draw() {
   background("#111C45");
-  image(backgrounds[index], 0, 75, 700, 425);
-  textSize(12);
+  image(backgrounds[index], 0, 135, 1600, 765);
+  textSize(24);
   textAlign(CENTER);
   fill("white");
-  text("Never in life have", 560, 100);
-  text("I blushed, Russians", 560, 110);
-  text("do not do this.", 560, 120);
-  text("You will", 115, 110);
-  text("murder me.", 115, 120);
-  text("Mr. Business Man,", 365, 440);
-  text("Mr. Landlord.", 365, 450);
+  text("Never in life have", 1280, 180);
+  text("I blushed, Russians", 1280, 198);
+  text("do not do this.", 1280, 216);
+  text("You will", 263, 198);
+  text("murder me.", 263, 216);
+  text("Mr. Business Man,", 834, 792);
+  text("Mr. Landlord.", 834, 810);
   noStroke();
   fill("#111C45");
-  rect(505, 75, 110, 60);
-  image(img16, 505, 75, 110, 60);
-  image(img17, 75, 75, 95, 70);
-  image(img18, 315, 400, 115, 70);
-  image(img19, 30, 10, 35, 35);
-  link.position(10, 45);
-  link.style("font-size", "10px");
+  rect(1154, 135, 251, 108);
+  image(img16, 1154, 135, 251, 108);
+  image(img17, 171, 135, 217, 126);
+  image(img18, 720, 720, 263, 126);
+  image(img19, 69, 18, 80, 63);
+  link.position(23, 81);
+  link.style("font-size", "20px");
   link.style("text-align", "CENTER");
   link.style("text-style", "NORMAL");
   link.style("color", "white");
@@ -53,8 +53,4 @@ function mousePressed() {
   if (index == backgrounds.length) {
     index = 0;
   }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
