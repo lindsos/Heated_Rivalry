daa5b2b2e32c42605739774c0916de14d6a968fe
lindsos <lindseystone04@gmail.com>
2026-04-10 00:58:43 -0400

    images as links

--- sketch.js
+++ sketch.js
@@ -6,19 +6,31 @@ function setup() {
   link4 = createA("index4.html", "EPISODE 4- ROSE");
   link5 = createA("index5.html", "EPISODE 5- ILL BELIEVE IN ANYTHING");
   link6 = createA("index6.html", "EPISODE 6- THE COTTAGE");
-  link11 = createA("index.html",
-    '<img src="assets/episode_1/episode_1.png", alt="episode_1">',);
-  link22 = createA("index.html",
-    '<img src="assets/episode_2/episode_2_background.png", alt="episode_2">',);
-  link33 = createA("index.html",
-    '<img src="assets/episode_3/episode_3_background_link.png", alt="episode_3">',);
-  link44 = createA("index.html",
-    '<img src="assets/episode_4/episode_4_background.png", alt="episode_4">',);
-  link55 = createA("index.html",
-    '<img src="assets/episode_5/episode_5_background.png", alt="episode_5">',);
-  link66 = createA("index.html",
-    '<img src="assets/episode_6/episode_6_background.png", alt="episode_6">',);
-  cursor('assets/HR.png');
+  link11 = createA(
+    "index.html",
+    '<img src="assets/episode_1/episode_1.png", alt="episode_1">',
+  );
+  link22 = createA(
+    "index.html",
+    '<img src="assets/episode_2/episode_2_background.png", alt="episode_2_background">',
+  );
+  link33 = createA(
+    "index.html",
+    '<img src="assets/episode_3/episode_3_background.png", alt="episode_3_background">',
+  );
+  link44 = createA(
+    "index.html",
+    '<img src="assets/episode_4/episode_4_background.png", alt="episode_4_background">',
+  );
+  link55 = createA(
+    "index.html",
+    '<img src="assets/episode_5/episode_5_background.png", alt="episode_5_background">',
+  );
+  link66 = createA(
+    "index.html",
+    '<img src="assets/episode_6/episode_6_background.png", alt="episode_6_background">',
+  );
+  cursor("assets/HR.png");
 }
 
 function draw() {
@@ -40,12 +52,11 @@ function draw() {
   link1.style("color", "black");
   link1.style("text-decoration", "none");
   link1.style("font-family", "zumme");
-  link11.position(-280, 11);
-  link11.style("transform", "scale(0.35)");
   noStroke();
   fill("#D9DDE9");
   rect(71, 225, 427, 281);
-  //image(img1, 57, 234, 434, 243);
+  link11.position(-280, 11);
+  link11.style("transform", "scale(0.35)");
 
   //episode 2
   link2.position(631, 550);
@@ -55,12 +66,11 @@ function draw() {
   link2.style("color", "black");
   link2.style("text-decoration", "none");
   link2.style("font-family", "zumme");
-  link22.position (241, 68);
-  link22.style("transform", "scale(0.35)");
   noStroke();
   fill("#111C45");
   rect(585, 225, 427, 281);
-  //image(img2, 571, 252, 434, 225);
+  link22.position(241, 68);
+  link22.style("transform", "scale(0.35)");
 
   //episode 3
   link3.position(1146, 550);
@@ -70,11 +80,11 @@ function draw() {
   link3.style("color", "black");
   link3.style("text-decoration", "none");
   link3.style("font-family", "zumme");
-  link33.position(1151, 267);
   noStroke();
   fill("#163355");
-  rect(1103, 225, 430, 281);
-  //image(img3, 1086, 225, 434, 252);
+  rect(1100, 224, 429, 282);
+  link33.position(748, 12.5);
+  link33.style("transform", "scale(0.35)");
 
   //episode 4
   link4.position(117, 887);
@@ -84,12 +94,11 @@ function draw() {
   link4.style("color", "black");
   link4.style("text-decoration", "none");
   link4.style("font-family", "zumme");
-  link44.position (67, 562);
-  link44.style("transform", "scale(0.35)");
   noStroke();
   fill("#F5FDFF");
-  rect(71, 562, 427, 281);
-  //image(img4, 57, 567, 434, 252);
+  rect(71, 567, 427, 274);
+  link44.position(-282, 352);
+  link44.style("transform", "scale(0.35)");
 
   //episode 5
   link5.position(631, 887);
@@ -99,12 +108,11 @@ function draw() {
   link5.style("color", "black");
   link5.style("text-decoration", "none");
   link5.style("font-family", "zumme");
-  link55.position (581, 562);
-  link55.style("transform", "scale(0.35)");
   noStroke();
   fill("#A3DEF8");
-  rect(585, 562, 427, 281);
-  //image(img5, 571, 567, 434, 252);
+  rect(585, 567, 427, 274);
+  link55.position(232, 350);
+  link55.style("transform", "scale(0.35)");
 
   //episode 6
   link6.position(1146, 887);
@@ -114,10 +122,9 @@ function draw() {
   link6.style("color", "black");
   link6.style("text-decoration", "none");
   link6.style("font-family", "zumme");
-  link66.position (1096, 582);
-  link66.style("transform", "scale(0.35)");
   noStroke();
   fill("#FDE9BD");
-  rect(1100, 562, 427, 281);
-  //image(img6, 1086, 585, 434, 234);
+  rect(1100, 567, 427, 274);
+  link66.position(750, 351);
+  link66.style("transform", "scale(0.35)");
 }
