44a47844d87b2b7997fbac66d85279a589c4ddd3
lindsos <lindseystone04@gmail.com>
2026-04-11 17:57:17 -0400

    gif 11

--- sketch4.js
+++ sketch4.js
@@ -7,7 +7,7 @@ let img28;
 function preload() {
   img25 = loadImage('/assets/episode_4/episode_4_background.png');
   img26 = loadImage('assets/episode_4/gif_10.png');
-  //img27 = loadImage('assets/episode_4/gif_11.png');
+  img27 = loadImage('assets/episode_4/gif_11.png');
   img28 = loadImage('assets/episode_4/gif_12.png');
   //All_the_Things_She_Said = loadSound("assets/episode_4/All_The_Things_She_Said_Heated_Rivalry_Version_tATu_Harrison.mp3");
 }
@@ -29,17 +29,17 @@ function draw() {
   textSize (24);
   textAlign (CENTER);
   fill ('black');
-  text('You want to make', 1045, 648);
-  text('me tuna melt?', 1045, 666);
-  //text('They say',365, 375);
-  //text('youre', 365, 385);
-  //text('boring too.', 365, 395);
-  //fill ('white');
+  text('You want to make', 1080, 648);
+  text('me tuna melt?', 1080, 666);
+  fill ('white');
+  text('They say', 910, 565);
+  text('youre', 910, 583);
+  text('boring too.', 910, 601);
   text('Have a fry,', 1291, 531);
   text('Shane', 1291, 549);
   text('Hollander.', 1291, 567);
-  image (img26, 949, 540, 240, 135);
-  //image (img27, 200, 175, 95, 65);
+  image (img26, 980, 540, 240, 135);
+  image (img27, 850, 545, 125, 171);
   image (img28, 1200, 477, 171, 171);
   link1.position(60, 130);
   link11.position(70, 18);
