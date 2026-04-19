5c9f6bfb4c65c79decd34fff2188dc61f9a405b3
lindsos <lindseystone04@gmail.com>
2026-04-18 22:27:55 -0400

    episode 6 gifs and hover

--- sketch6.js
+++ sketch6.js
@@ -3,11 +3,15 @@ let sunHeight = 1440;
 let redVal = 0;
 let greenVal = 0;
 let img09;
+//let img16;
+let img17;
 let img18;
 
 function preload() {
-  img09 = loadImage('/assets/episode_6/episode_6_background.png');
-  img18 = loadImage('/assets/episode_6/gif_18.png');
+  img09 = loadImage('assets/episode_6/episode_6_background.png');
+  //img16 = loadImage('assets/episode_6/gif_16.png);
+  img17 = loadImage('assets/episode_6/gif_17.png');
+  img18 = loadImage('assets/episode_6/gif_18.png');
 }
 
 function setup() {
@@ -21,6 +25,7 @@ function setup() {
     '<img src="assets/tv_remote.png", alt="tv_remote">',
   );
   //cursor('assets/HR.png');
+
 }
 
 function draw() {
@@ -36,14 +41,44 @@ function draw() {
     redVal += 4;
     greenVal += 1;
   }
-  frameRate(12);
-  textSize(24);
-  text("X: "+mouseX, 200, 100);
-  text("Y: "+mouseY, 50, 200);
 }
-  console.log(sunHeight);
   image(img09, 0 * ratio, 77 * ratio, 1440 * ratio, 947 * ratio);
+  //image (img16, 0 * ratio, 0 * ratio);
+
+
+  image (img17, 145 * ratio, 640 * ratio);
+    if (mouseX > 120 && mouseX < 300 && mouseY > 555 && mouseY < 735) {
+    isHovering = true;
+  } else {
+    isHovering = false;
+  }
+  if (isHovering) {
+    fill(0, 150);
+    rect(120, 552, 147, 187);
+    fill("white");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("Ya tebya", 193, 635);
+    text("lyublyu.", 193, 655);
+  }
+  
   image (img18, 800 * ratio, 825 * ratio);
+    if (mouseX > 700 && mouseX < 875 && mouseY > 720 && mouseY < 885) {
+    isHovering = true;
+  } else {
+    isHovering = false;
+  }
+  if (isHovering) {
+    fill(0, 150);
+    rect(695, 715, 185, 175);
+    fill("white");
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("Since rookie year.", 790, 785);
+    text("(The summer", 790, 805);
+    text("before).", 790, 825);
+  }
+
   link1.position(5 * ratio, 0 * ratio);
   link11.position(0 * ratio, 0 * ratio);
   link11.style("transform", "scale(0.5)");
@@ -53,12 +88,17 @@ function draw() {
   link1.style("color", "black");
   link1.style("text-decoration", "none");
   link1.style("font-family", "helvetica-neue-lt-pro");
-}
 
   if (mouseIsPressed===true && sunHeight === 130) {
-    background(0);
+  background(0);
   }
 
+  frameRate(12);
+  textSize(24);
+  text("X: "+mouseX, 200, 100);
+  text("Y: "+mouseY, 50, 200);
+
+}
 
 function windowResized() {
   const design_w = 1440;
